b52a9f737dc4dc77b00bebb5bd36974ae72caa63
Emma M <ekmilner@gmail.com>
2015-11-21 18:00:37 +0000

    adds endpoint to fetch data for PMQs

--- server.js
+++ server.js
@@ -16,16 +16,30 @@ server.register(require("inert"), function (err) {
         throw err;
     }
 
-    server.route({
-        method: "GET",
-        path: "/{param*}",
-        handler: {
-            directory: {
-                path: "./public",
-                listing: true
+    server.route([
+        {
+            method: "GET",
+            path: "/{param*}",
+            handler: {
+                directory: {
+                    path: "./public",
+                    listing: true
+                }
             }
+        },
+        {
+            method: "GET",
+            path: "/data",
+            handler: function(req, rep){
+                        var Wreck = require('wreck');
+                        var uri = 'http://lda.data.parliament.uk/answeredquestions.json?AnsweringBody=Prime%20Minister';
+
+                        Wreck.get(uri, function (err, res, payload) {
+                            rep(payload);
+                    });
+                }
         }
-    });
+    ]);
 
     server.start(function () {
         console.log("Server running at: http://localhost:" + server.info.port); //eslint-disable-line no-console
